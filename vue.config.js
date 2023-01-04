d393e7048fefffdfb3e095960f1ea6915fb16415
Doan_na <doanna@mpos.vn>
2023-01-04 08:56:45 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue2-example/' // Thay tên repository của các bạn vào đây nhé
+    : '/'
+}
