8bd295cdf8f3377b5946db37b105f11bbce99549
Doan_na <doanna@mpos.vn>
2023-01-05 15:37:36 +0700

    update address

--- src/utils/address/index.js
+++ src/utils/address/index.js
@@ -1,9 +1,35 @@
+import { removeVietnameseTones } from '../function'
 import citiesRaw from './cities.json'
+import districtsRaw from './districts.json'
+import wardsRaw from './wards.json'
 
-let cities = []
-
-export function getAllCity() {
-  if (cities) return cities
-  cities = JSON.parse(citiesRaw)
-  return cities
+export function getListCity() {
+  return citiesRaw.map(item => {
+    item.textSearch = removeVietnameseTones(item.name).toLowerCase() 
+    return item
+  }).sort((a,b) => a.code - b.code)
+}
+export function getListDistrict(cityCode) {
+  if(cityCode) {
+    let result = []
+  
+    result = districtsRaw.filter(item => item.parent_code == cityCode)
+    result = result.map(item => {
+      item.textSearch = removeVietnameseTones(item.name).toLowerCase() 
+      return item
+    }).sort((a,b) => a.code - b.code)
+    return result
+  } else return []
+}
+export function getListWard(districtCode) {
+  if(districtCode) {
+    let result = []
+  
+    result = wardsRaw.filter(item => item.parent_code == districtCode)
+    result = result.map(item => {
+      item.textSearch = removeVietnameseTones(item.name).toLowerCase() 
+      return item
+    }).sort((a,b) => a.code - b.code)
+    return result
+  } else return []
 }
--- src/views/DemoForm.vue
+++ src/views/DemoForm.vue
@@ -14,25 +14,61 @@
         <el-input v-model.number="ruleForm.age"></el-input>
       </el-form-item>
       <el-form-item label="City" prop="city">
-        <el-select value-key="code" v-model="value" placeholder="Select">
-          <el-option
-            v-for="item in listCity"
-            :key="item.code"
-            :label="item.label"
-            :value="item.name">
-          </el-option>
+        <el-select
+          @visible-change="visibleCityInput"
+          @change="selectCity"
+          class="w-100"
+          :filter-method="searchCity"
+          filterable
+          value-key="code"
+          v-model="ruleForm.city"
+          placeholder="Select"
+        >
+          <el-option v-for="item in listCityFilter" :key="item.code" :label="item.name" :value="item"> </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="District" prop="district">
+        <el-select
+          ref="refSelectDistrict"
+          @visible-change="visibleDistrictInput"
+          @change="selectDistrict"
+          class="w-100"
+          :filter-method="searchDistrict"
+          filterable
+          value-key="code"
+          v-model="ruleForm.district"
+          placeholder="Select"
+        >
+          <el-option v-for="item in listDistrictFilter" :key="item.code" :label="item.name" :value="item"> </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Ward" prop="ward">
+        <el-select
+          ref="refSelectWard"
+          @visible-change="visibleWardInput"
+          class="w-100"
+          :filter-method="searchWard"
+          filterable
+          value-key="code"
+          v-model="ruleForm.ward"
+          placeholder="Select"
+        >
+          <el-option v-for="item in listWardFilter" :key="item.code" :label="item.name" :value="item"> </el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">Submit</el-button>
         <el-button @click="resetForm('ruleForm')">Reset</el-button>
       </el-form-item>
-      
     </el-form>
   </div>
 </template>
 
 <script>
+import { getListCity, getListDistrict, getListWard } from "../utils/address/index.js";
+import { removeVietnameseTones } from "../utils/function.js";
+import _ from "lodash";
+
 export default {
   data() {
     var checkAge = (rule, value, callback) => {
@@ -45,6 +81,8 @@ export default {
         } else {
           if (value < 18) {
             callback(new Error("Phải lớn hơn 18 tủi"));
+          } else if (value > 80) {
+            callback(new Error("Quá tuổi rồi"));
           } else {
             callback();
           }
@@ -90,25 +128,107 @@ export default {
         checkPass: "",
         age: "",
         city: null,
+        district: null,
+        ward: null,
       },
       rules: {
-        name: [{ validator: validateName, trigger: "blur" }],
-        pass: [{ validator: validatePass, trigger: "blur" }],
-        checkPass: [{ validator: validatePass2, trigger: "blur" }],
-        age: [{ validator: checkAge, trigger: "blur" }],
-        city: [{ required: true, message: 'Please select Activity zone', trigger: 'change' }],
+        name: [{ required: true, validator: validateName, trigger: "blur" }],
+        pass: [{ required: true, validator: validatePass, trigger: "blur" }],
+        checkPass: [{ required: true, validator: validatePass2, trigger: "blur" }],
+        age: [{ required: true, validator: checkAge, trigger: "blur" }],
+        city: [{ required: true, message: "Chọn Tỉnh/TP", trigger: "change" }],
+        district: [{ required: true, message: "Chọn Quận, Huyện", trigger: "change" }],
+        ward: [{ required: true, message: "Chọn Xã, Phường", trigger: "change" }],
       },
       listCity: [],
+      listCityFilter: [],
+      listDistrict: [],
+      listDistrictFilter: [],
+      listWard: [],
+      listWardFilter: [],
     };
   },
   created() {
-
+    this.getAddress();
   },
   methods: {
+    async getAddress() {
+      this.listCity = await getListCity();
+      this.listCityFilter = _.cloneDeep(this.listCity);
+    },
+    // city
+    visibleCityInput(isShow) {
+      if (isShow) {
+        this.listCityFilter = _.cloneDeep(this.listCity);
+      }
+    },
+    searchCity(query) {
+      const textSearch = removeVietnameseTones(query.trim().toLocaleLowerCase());
+      if (textSearch) {
+        this.listCityFilter = this.listCity.filter((item) => item.textSearch.includes(textSearch));
+      } else {
+        this.listCityFilter = _.cloneDeep(this.listCity);
+      }
+    },
+    selectCity(city) {
+      this.resetDistrict();
+      this.resetWard();
+      this.listDistrict = getListDistrict(city.code);
+      this.listDistrictFilter = _.cloneDeep(this.listDistrict);
+      this.$refs.refSelectDistrict.focus();
+    },
+
+    // district
+    visibleDistrictInput(isShow) {
+      if (isShow) {
+        this.listDistrictFilter = _.cloneDeep(this.listDistrict);
+      }
+    },
+    searchDistrict(query) {
+      const textSearch = removeVietnameseTones(query.trim().toLocaleLowerCase());
+      if (textSearch) {
+        this.listDistrictFilter = this.listDistrict.filter((item) => item.textSearch.includes(textSearch));
+      } else {
+        this.listDistrictFilter = _.cloneDeep(this.listDistrict);
+      }
+    },
+    selectDistrict(district) {
+      this.resetWard();
+      this.listWard = getListWard(district.code);
+      this.listWardFilter = _.cloneDeep(this.listWard);
+      this.$refs.refSelectWard.focus();
+    },
+
+    // ward
+    visibleWardInput(isShow) {
+      if (isShow) {
+        this.listWardFilter = _.cloneDeep(this.listWard);
+      }
+    },
+    searchWard(query) {
+      const textSearch = removeVietnameseTones(query.trim().toLocaleLowerCase());
+      if (textSearch) {
+        this.listWardFilter = this.listWard.filter((item) => item.textSearch.includes(textSearch));
+      } else {
+        this.listWardFilter = _.cloneDeep(this.listWard);
+      }
+    },
+
+    resetDistrict() {
+      this.listDistrict = [];
+      this.listDistrictFilter = [];
+      this.ruleForm.district = null;
+    },
+    resetWard() {
+      this.listWard = [];
+      this.listWardFilter = [];
+      this.ruleForm.ward = null;
+    },
+
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert("submit!");
+          alert("ok " + JSON.stringify(this.ruleForm));
         } else {
           console.log("error submit!!");
           return false;
